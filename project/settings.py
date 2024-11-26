413ac3a8e6c50b6764a8ccc7f5fd5f5edb6dead7
Temirlan Kazhikarimov <041026551702@stud.satbayev.university>
2024-11-26 17:44:55 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False)) # False по умолчанию
 
 
 # Quick-start development settings - unsuitable for production
